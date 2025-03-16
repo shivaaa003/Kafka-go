a3f9e8bf8fc1ab0b4135557248f985e153e5b4af
shivaaa003 <shivamsaini00300@gmail.com>
2025-03-17 02:31:25 +0530

    returning simple v0 response

--- app/main.go
+++ app/main.go
@@ -10,20 +10,46 @@ import (
 var _ = net.Listen
 var _ = os.Exit
 
+func handleConnection(connection net.Conn) {
+	defer connection.Close()
+
+	buffer := make([]byte, 1024)
+	n, err := connection.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading request: ", err.Error())
+	}
+	fmt.Println("Recieved Data: ", string(buffer[:n]))
+
+	// buffer = make([]byte, 1024)
+	buffer = []byte("1\r\n \r\n7")
+	n, err = connection.Write(buffer)
+	if err != nil {
+		fmt.Println("Error writing response: ", err.Error())
+	}
+	fmt.Println("Send Data: ", string(buffer[:n]))
+
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment this block to pass the first stage
-	
+
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	defer l.Close()
+
+	for {
+		connection, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(connection)
 	}
 }
