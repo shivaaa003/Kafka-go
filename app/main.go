11edd2c7fce973b2f90401d7af5cf47972a222b0
shivaaa003 <shivamsaini00300@gmail.com>
2025-03-17 02:45:25 +0530

    continuosly reading until is errors out

--- app/main.go
+++ app/main.go
@@ -9,61 +9,63 @@ import (
 )
 
 func handleConnection(connection net.Conn) {
-	
+	defer connection.Close()
 
-	buffer := make([]byte, 1024)
-	_, err := connection.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading request: ", err.Error())
+	for {
+		buffer := make([]byte, 1024)
+		_, err := connection.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading request: ", err.Error())
+			return
+		}
+		// fmt.Println("Recieved Data: ", string(buffer[:n]))
+
+		// ----------- New Method -------------
+
+		// request := parseRequest(buffer)
+		// fmt.Println(request)
+
+		// ----------- Old Method -------------
+		messageSize := make([]byte, 4)
+
+		correlationID := buffer[8 : 8+4]
+		// fmt.Println("CorelationID: ", binary.BigEndian.Uint32(correlationID))
+		apiVersion := buffer[6 : 6+2]
+
+		errorCode := make([]byte, 2)
+		switch binary.BigEndian.Uint16(apiVersion) {
+		case 0, 1, 2, 3, 4:
+			binary.BigEndian.PutUint16(errorCode, uint16(0))
+		default:
+			binary.BigEndian.PutUint16(errorCode, uint16(35))
+		}
+
+		apiKeys := []byte{2, 0, 18, 0, 0, 0, 4}
+
+		tagBuffer := byte(0)
+		throttleTime := make([]byte, 4)
+		binary.BigEndian.PutUint32(throttleTime, uint32(0))
+		// buffer = make([]byte, 1024)
+		message := correlationID
+		message = append(message, errorCode...)
+		message = append(message, apiKeys...)
+		message = append(message, tagBuffer)
+		message = append(message, throttleTime...)
+		message = append(message, tagBuffer)
+
+		binary.BigEndian.PutUint32(messageSize, uint32(len(message)))
+
+		bBuffer := bytes.Buffer{}
+		bBuffer.Write(messageSize)
+		bBuffer.Write(message)
+		// Refer for message response structure: https://forum.codecrafters.io/t/question-about-handle-apiversions-requests-stage/1743/3?u=ganimtron-10
+
+		_, err = connection.Write(bBuffer.Bytes())
+		if err != nil {
+			fmt.Println("Error writing response: ", err.Error())
+		}
+		// fmt.Println("Send Data: ", string(bBuffer.Bytes()[:n]))
 	}
-	// fmt.Println("Recieved Data: ", string(buffer[:n]))
-
-	// ----------- New Method -------------
-
-	// request := parseRequest(buffer)
-	// fmt.Println(request)
-
-	// ----------- Old Method -------------
-	messageSize := make([]byte, 4)
-
-	correlationID := buffer[8 : 8+4]
-	// fmt.Println("CorelationID: ", binary.BigEndian.Uint32(correlationID))
-	apiVersion := buffer[6 : 6+2]
-
-	errorCode := make([]byte, 2)
-	switch binary.BigEndian.Uint16(apiVersion) {
-	case 0, 1, 2, 3, 4:
-		binary.BigEndian.PutUint16(errorCode, uint16(0))
-	default:
-		binary.BigEndian.PutUint16(errorCode, uint16(35))
-	}
-
-	apiKeys := []byte{2, 0, 18, 0, 0, 0, 4}
-
-	tagBuffer := byte(0)
-	throttleTime := make([]byte, 4)
-	binary.BigEndian.PutUint32(throttleTime, uint32(0))
-	// buffer = make([]byte, 1024)
-	message := correlationID
-	message = append(message, errorCode...)
-	message = append(message, apiKeys...)
-	message = append(message, tagBuffer)
-	message = append(message, throttleTime...)
-	message = append(message, tagBuffer)
-
-	binary.BigEndian.PutUint32(messageSize, uint32(len(message)))
-
-	bBuffer := bytes.Buffer{}
-	bBuffer.Write(messageSize)
-	bBuffer.Write(message)
-	// Refer for message response structure: https://forum.codecrafters.io/t/question-about-handle-apiversions-requests-stage/1743/3?u=ganimtron-10
-
-	_, err = connection.Write(bBuffer.Bytes())
-	if err != nil {
-		fmt.Println("Error writing response: ", err.Error())
-	}
-	// fmt.Println("Send Data: ", string(bBuffer.Bytes()[:n]))
-
 }
 
 func main() {
